33c2108480fc88866ac680be728523eca618448e
minhtanLT123 <111tan777@gmail.com>
2025-11-20 21:10:48 +0700

    layout lại danh sách ghế full

--- src/pages/MovieBooking.jsx
+++ src/pages/MovieBooking.jsx
@@ -5,17 +5,20 @@ export default function MovieBooking() {
     const selectedSeats = useSelector((s) => s.seats.selectedSeats);
 
     return (
-        <div className="p-10">
+        <div className="container text-center">
             <h1 className="text-2xl font-bold mb-4">Đặt ghế xem phim</h1>
+            <div className="p-10 grid grid-cols-2 gap-3">
 
-            <SeatSelector />
 
-            <div className="mt-6 p-4 bg-gray-100 rounded">
-                <h2 className="font-semibold">Ghế đã chọn:</h2>
-                <p>{selectedSeats.length ? selectedSeats.join(", ") : "Chưa chọn"}</p>
-                <p className="mt-2 font-bold">
-                    Tổng tiền: {selectedSeats.length * 80000} VND
-                </p>
+                <SeatSelector />
+
+                <div className="mt-6 p-4 bg-gray-100 rounded">
+                    <h2 className="font-semibold">Ghế đã chọn:</h2>
+                    <p>{selectedSeats.length ? selectedSeats.join(", ") : "Chưa chọn"}</p>
+                    <p className="mt-2 font-bold">
+                        Tổng tiền: {selectedSeats.length * 80000} VND
+                    </p>
+                </div>
             </div>
         </div>
     );
